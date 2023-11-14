73f5be3189060dc88e124be9f25d7501b524a52d
Emilio Oropeza <vocho17@gmail.com>
2023-11-14 13:20:16 -0600

    Add changes styles and images

--- js/result.js
+++ js/result.js
@@ -2,12 +2,12 @@
         export const result = {
             "a":{
                 "title" : " Lo tuyo es #TecateRogelia.",
-                "description" : " ",
+                "description" : "Porqué eres amante de sabores extravagantes con toques picositos y con cuerpo robusto.",
                 "image": "Rogelia imagen resultado final.png",
             },
             "b":{
                 "title" : "Una #TecateGraciela para ti",
-                "description" : " ",
+                "description" : "Porqué eres amante de bebidas ligeras y con toques que refresquen tu paladar.",
                 "image": "Graciela imagen resultado final.png",
             },
         }
